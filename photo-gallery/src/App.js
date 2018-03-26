ee2c407e509112860970b5e2766d7ff678c9d5fd
Nicolas Suarez-Canton Trueba <nsuarez.canton@gmail.com>
2018-03-27 00:09:57 +0200

    Initial Commit

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class Comment extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
